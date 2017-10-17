240f064ecaf680fe7d3d1098dc1928292c1521dc
Ibbus93 <ibbus93@gmail.com>
2017-10-17 18:32:52 +0200

    Loading the first test file

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName("Project").setMaster("local")
 sc = SparkContext(conf=conf)
 
+twitter2015testBD = sc.textFile("tweets_subtask_BD/twitter-2015test-BD-down.tsv").map(lambda line : line.split("\t")).collect()
