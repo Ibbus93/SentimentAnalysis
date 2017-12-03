0da53e60de206ef85a7e7644eb0d778e06215b2c
Ibbus <ibbus93@gmail.com>
2017-12-03 11:46:59 +0100

    Preprocessing done

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from pyspark import SparkContext, SparkConf
-from pyspark.sql import SQLContext, HiveContext, SparkSession
-from pyspark.sql import functions as fn
+from pyspark.sql import HiveContext, SparkSession
+from pyspark.sql import SQLContext
+from pyspark.ml.feature import StopWordsRemover
+from pyspark.ml.feature import CountVectorizer
+from pyspark.ml.feature import RegexTokenizer
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import IDF
+
+import requests
 import utilities
 
 # Configurazioni varie
 conf = SparkConf().setAppName("Project").setMaster("local")
 sc = SparkContext(conf=conf)
-sqlContext = HiveContext(sc)
+sqlContext = SQLContext(sc)
 spark = SparkSession(sc)
 
 # t2015train = loadTsv("/home/fede/PycharmProjects/SentimentAnalysis/tweets_subtask_BD/twitter-2015train-BD-down.tsv")
@@ -14,8 +21,46 @@ spark = SparkSession(sc)
 # Carico i file per la challenge B che servono al machine learning scremandoli di quelli non disponibili
 t2015train = utilities.loadTsv("tweets_subtask_BD/twitter-2015train-BD-down.tsv", sc)
 
+
+for x in t2015train.collect():
+    print(x)
+
 # Creo i data frame dal file di training
 train_df = t2015train.toDF(['tweet_id', 'topic', 'classification', 'tweet_text'])
 train_df.printSchema()
 
-# https://twitter.com/statuses/
+print(train_df.show(10))
+
+# Carico le stop words e le rimuovo
+stop_words = requests.get('http://ir.dcs.gla.ac.uk/resources/linguistic_utils/stop_words').text.split()
+stop_words = [x.encode('UTF8') for x in stop_words]
+
+
+sw_filter = StopWordsRemover()\
+  .setStopWords(stop_words)\
+  .setCaseSensitive(False)\
+  .setInputCol("words")\
+  .setOutputCol("filtered")
+
+# Computo il IDF su tutte le parole
+
+cv = CountVectorizer(minTF=1., minDF=5., vocabSize=2**17)\
+  .setInputCol("filtered")\
+  .setOutputCol("tf")
+
+tokenizer = RegexTokenizer().setGaps(False)\
+  .setPattern("\\p{L}+")\
+  .setInputCol("tweet_text")\
+  .setOutputCol("words")
+
+review_words_df = tokenizer.transform(train_df)
+
+cv_pipeline = Pipeline(stages=[tokenizer, sw_filter, cv]).fit(train_df)
+print(cv_pipeline.transform(train_df).show(5))
+
+idf = IDF().\
+    setInputCol('tf').\
+    setOutputCol('tfidf')
+
+idf_pipeline = Pipeline(stages=[cv_pipeline, idf]).fit(train_df)
+print(idf_pipeline.transform(train_df).show(5))
--- utilities.py
+++ utilities.py
@@ -1,19 +1,25 @@
 def deleteNotAvailable(tweets):
     av = []
     for tweet in tweets:
-        if tweet[3] != 'Not Available':
+        if tweet[3] != 'Not Available' and tweet[2] != 'off topic':
             av.append(tweet)
     return av
 
 def loadTsv(file, sc):
-    return sc.parallelize(
-        deleteNotAvailable(
-            sc.textFile(file)\
-            .map(lambda x : x.encode("ascii", "ignore"))\
-            .map(lambda line : line.split("\t"))\
+    aux = deleteNotAvailable(
+        sc.textFile(file) \
+            .map(lambda x: x.encode("ascii", "ignore")) \
+            .map(lambda line: line.split("\t")) \
             .collect()
-        )
     )
+
+    # Trasformo l'attributo categorico
+    score = {'negative': -1, 'neutral': 0, 'positive': 1}
+    for x in aux:
+        x[2] = score[x[2]]
+
+    return sc.parallelize(aux)
+    # return aux
 
 # t2015test = utilities.loadTsv("tweets_subtask_BD/twitter-2015test-BD-down.tsv", sc)
 # t2016dev = utilities.loadTsv("tweets_subtask_BD/twitter-2016dev-BD-down.tsv", sc)
